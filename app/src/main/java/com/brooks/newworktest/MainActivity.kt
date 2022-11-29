2be067cf74c47cc1736b85f15f26ac831d50f3fa
brooks <myt2000@126.com>
2022-11-29 10:20:14 +0800

    访问百度,获取html页面内容

--- app/src/main/java/com/brooks/newworktest/MainActivity.kt
+++ app/src/main/java/com/brooks/newworktest/MainActivity.kt
@@ -2,10 +2,57 @@ package com.brooks.newworktest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.CacheResponse
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sendRequestBtn.setOnClickListener {
+            sendRequestWithHttpURLConnection()
+        }
+    }
+
+
+    private fun sendRequestWithHttpURLConnection() {
+        // 开启线程发起网络请求
+        thread {
+
+            var connection: HttpURLConnection? = null
+
+            try {
+                val response = StringBuilder()
+                val url = URL("https://www.baidu.com")
+                connection = url.openConnection() as HttpURLConnection
+                connection.connectTimeout = 8000
+                connection.readTimeout = 8000
+                val input = connection.inputStream
+                // 下面对获取到的输入流进行读取
+                val reader = BufferedReader(InputStreamReader(input))
+                reader.use {
+                    reader.forEachLine {
+                        response.append(it)
+                    }
+                }
+                showRespones(response.toString())
+            } catch (e: java.lang.Exception) {
+                e.printStackTrace()
+            } finally {
+                connection?.disconnect()
+            }
+        }
+    }
+
+    private fun showRespones(response: String) {
+        runOnUiThread {
+            // 在这里进行UI操作，将结果显示到界面上
+            responseText.text = response
+        }
     }
 }
